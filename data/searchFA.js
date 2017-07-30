d5e5709b75dd9294135371d7d20b3edce712cc9a
Harrison Wynn <harrisonwynn42@gmail.com>
2017-07-29 22:49:57 -0500

    searching changed a bit

--- data/searchFA.js
+++ data/searchFA.js
@@ -2,14 +2,25 @@
 var term = "modular";
 //puts words in the input box
 var titlePort = function(x){
-	var zA = document.querySelector("form#search-form fieldset input#q");
+	// var zA = document.querySelector("form#search-form fieldset input#q");
+	var zA = document.querySelector("#JSMessage");
 	zA.value = x;
 };
 //hits the submit button
 var ShipIt = function(){
-document.querySelector("form#search-form").submit();
+var b = document.querySelector("input.button:nth-child(3)");
+b.name = "finish";
+//document.querySelector("form#search-form").submit();
+document.querySelector("#JSForm").submit();
+};
+var EndIt = function(){
+document.addEventListener("submit", function(event) {
+console.log("Unloading now");
+self.port.emit("myMessage");
+}, false);
 };
 var Finalize = function(){
+EndIt();
 titlePort(term);
 ShipIt();
 };
--- data/searchIB.js
+++ data/searchIB.js
@@ -1,11 +1,29 @@
-var term = "sample";
-
-//get search area
-var IBsearch = document.querySelector("#text");
-//get submit button
-var IBsubmit  = document.querySelector("body > form:nth-child(12)");
-
-//insert search term
-IBsearch.value = term;
+var term = "modular";
+//puts words in the input box
+var titlePort = function(x){
+	var zA = document.querySelector("#text");
+	zA.value = x;
+};
 //hits the submit button
-IBsubmit.submit();
+var ShipIt = function(){
+	// document.querySelector("body > form:nth-child(12)").submit();
+	// document.querySelector("body > form:nth-child(9)").submit();
+	var x = document.querySelector("body > form:nth-child(12)");
+	var y =document.querySelector("body > form:nth-child(9)");
+	if (x !==null || y !==null)
+	{
+		if (x !== null) 
+		{
+			x.submit();
+		}
+		else 
+		{
+			y.submit();
+		}
+	}
+};
+var Finalize = function(){
+titlePort(term);
+ShipIt();
+};
+Finalize();
