eb3e056869748c19f93da540ce922cd4f508b15f
Harrison Wynn <harrisonwynn42@gmail.com>
2017-07-29 22:38:49 -0500

    main.js: 2015-Mar-02 06:47pm

--- data/change.js
+++
@@ -1,6 +0,0 @@
-console.log("change.js is running");                                                                                            
-console.log(self.options.a.urls[1]);
-window.addEventListener('click', function(event) {
-console.log("Changes.js");
-self.port.emit("myMessage");
-}, false);
